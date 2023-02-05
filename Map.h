1a92ca25b4481f6f54bb21b9b4ce70f28baade7e
Rahmath007 <rahmath207@gmail.com>
2023-02-05 15:52:41 +0000

    display map in the console

--- Map.h
+++ Map.h
@@ -1,1 +1,22 @@
 #pragma once
+
+#include "Header.h"
+using namespace std;
+
+
+class Map
+{
+	// create map
+public:
+	Map(int width, int height) : width(width), height(height)
+	{
+		map = new char* [height];
+		for (int i = 0; i < height; i++)
+		{
+			map[i] = new char[width];
+			for (int j = 0; j < width; j++)
+			{
+				map[i][j] = '.';
+			}
+		}
+	}
