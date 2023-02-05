c0ee9f52c276ad372c87393f230a970ddaf5adf7
Rahmath007 <rahmath207@gmail.com>
2023-02-05 15:36:22 +0000

    mosnter class with constructor

--- Monster.h
+++ Monster.h
@@ -1,1 +1,16 @@
 #pragma once
+
+
+using namespace std;
+
+
+class Monster : public Entity
+{
+public:
+	Monster(int x, int y) : Entity(x, y), health(50), attack(15), defense(5) {}
+
+	int health;
+	int attack;
+	int defense;
+};
+
