7f3f7a260377735f29505a2a74eace6e98355d8d
Rahmath007 <rahmath207@gmail.com>
2023-02-05 15:25:11 +0000

    cretad player class with constructor

--- Player.h
+++ Player.h
@@ -1,1 +1,14 @@
 #pragma once
+
+using namespace std;
+
+class Player {
+public:
+	Player(int x, int y), health(100), attack(20), defense(10) {}
+
+	int health;
+	int attack;
+	int defence;
+	int gold = 0;
+	int experirnce = 0;
+};
